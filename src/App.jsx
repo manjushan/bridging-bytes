aa23b4efd727d0f31a70fd8b7761dde38b40a583
manjushan <manjushan12@gmail.com>
2025-10-05 18:24:32 -0700

    Add Bridging Bytes pages: Home, About, Join, Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Work from "./pages/Work";
-import GetInvolved from "./pages/GetInvolved";
+import Join from "./pages/Join";
 import Contact from "./pages/Contact";
 import Donate from "./pages/Donate";
 
@@ -12,15 +11,13 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900">
       <Navbar />
-      <div className="mt-16">
+      <div className="p-6">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/get-involved" element={<GetInvolved />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/donate" element={<Donate />} />
-        </Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/join" element={<Join />} />
+       <Route path="/contact" element={<Contact />} /> 
+       </Routes>
       </div>
     </div>
   );
